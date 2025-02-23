1f0514c2483cf756728c91ae3f0ca4ac921d233d
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-02-23 11:43:59 +0530

    connect mongodb database

--- src/main/java/com/cabbooking/cab_booking_system/Config/MongoDBConnection.java
+++ src/main/java/com/cabbooking/cab_booking_system/Config/MongoDBConnection.java
@@ -1,4 +1,37 @@
 package com.cabbooking.cab_booking_system.Config;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public class MongoDBConnection {
+    private static MongoDBConnection instance;
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DATABASE_NAME = "cab_booking";
+
+    private MongoDBConnection() {
+        mongoClient = MongoClients.create(CONNECTION_STRING);
+        database = mongoClient.getDatabase(DATABASE_NAME);
+    }
+
+    public static synchronized MongoDBConnection getInstance() {
+        if (instance == null) {
+            instance = new MongoDBConnection();
+        }
+        return instance;
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+
+    public void closeConnection() {
+        if (mongoClient != null) {
+            mongoClient.close();
+            instance = null;
+        }
+    }
 }
